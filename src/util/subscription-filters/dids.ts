40ae68cc2e5af610ed7436695e032977a73caf59
Pedro Fracassi <pedroluizfracassi@gmail.com>
2024-09-04 11:10:08 -0300

    conta do cblol

--- src/util/subscription-filters/dids.ts
+++ src/util/subscription-filters/dids.ts
@@ -1,3 +1,4 @@
 export default [
-  'did:plc:bqfs3unautrlgvjickb54y55' // ilha das lendas
+  'did:plc:uk7niqvqdgr74qhwb4vrg3vi', // conta oficial do cblol
+  'did:plc:bqfs3unautrlgvjickb54y55', // ilha das lendas
 ]
